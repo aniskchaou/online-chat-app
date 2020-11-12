107cd3c3f17898abd1b626ff4a601ab5cc898152
Anis KCHAOU <kchaouanis25@gmail.com>
2020-11-12 11:55:59 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer('someid',{
-  host: 'meetix.herokuapp.com',
+const myPeer = new Peer({
+  host: '/',
   port: '9000',
   path:'/'
 })
