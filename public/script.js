c2282873ab353ce1f7cc8e323ef84b2d625332b2
anis kchaou <aniskchaou>
2020-11-12 07:37:07 -0800

    initial commit

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
+  path: '/peer'
   port: '443'
 })
 const myVideo = document.createElement('video')
